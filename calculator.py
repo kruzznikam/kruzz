6b5c2c988aa334ce14c89baf94d8d9e04ae3b9f5
kruzznikam <59591710+kruzznikam@users.noreply.github.com>
2020-02-11 18:38:25 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,28 @@
 def add(x , y):
+  return x+y
+
+def sub(x , y):
+  return x-y
+
+def mul(x , y):
+  return x*y
+
+def div(x , y):
+  return x/y
+
+print("Select option , 1 2 3 4")
+
+choice = input("Enter option")
+
+num1 = float(input("Enter Number 1"))
+num2 = float(input("Enter Number 2'))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", sub(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", mul(num1,num2))
+elif choice == '4':
+   print(num1,"/",num2,"=", div(num1,num2))
+else:
+   print("Invalid input")
